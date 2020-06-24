b5c1a7950f4c59b3849e277aa492244b251a807c
Subramanian Chockalingam <subbuc@yahoo.com>
2020-06-25 00:47:09 +0530

    Initial Project Setup

---
+++ ToDoList/ToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Subramanian Chockalingam on 25/06/20.
+//  Copyright © 2020 Subramanian Chockalingam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
